9fa061b8d0c54c57315501c338d8ae154930e7a7
amh21-Ha <amha.ha07@gmail.com>
2022-12-01 03:21:11 -0800

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from sys import argv
     sum_var = 0
     for i in range(1, len(argv)):
-       sum_var += int(argv[i])
+        sum_var += int(argv[i])
     print("{}".format(sum_var))
