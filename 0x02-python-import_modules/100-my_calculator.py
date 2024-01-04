1ffc5a7e0e4fe2d19d14855bd17aa0bcad0636cf
Amha <amha.ha07@gmail.com>
2024-01-04 04:20:41 -0800

    my calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,12 +3,12 @@ if __name__ == "__main__":
     """
     Handle basic arithmetic operations.
     """
-    import calculator_1 #import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    ops = {"+": calculator_1.add, "-": calculator_1.sub, "*": calculator_1.mul, "/": calculator_1.div}
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
