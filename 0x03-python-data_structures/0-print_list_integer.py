107f21343e26b4df363a55b9b89682bda6ba1d77
Amha <amha.ha07@gmail.com>
2024-01-04 23:00:48 -0800

    print integer list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # 0-print_list_integer.py
-
+#  list only contains integers
 def print_list_integer(my_list=[]):
     """Print all integers of a list."""
     for i in range(len(my_list)):
