30a2c10d4daba7a0fe0979b50c0602a95fdc7d89
Amha <amha.ha07@gmail.com>
2024-01-06 11:59:10 -0800

    element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,6 @@
 def element_at(list, idx):
     if idx > (len(list) - 1) and inx < 0:
         return None
-    idx = list[idx]
-    return idx
+    else:
+        idx = list[idx]
+        return idx
