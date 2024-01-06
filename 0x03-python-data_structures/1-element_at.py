d4ba1c8407f1476ab1289519d95cdb0821cf0c70
Amha <amha.ha07@gmail.com>
2024-01-06 12:35:01 -0800

    retrive elements from list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx > (len(my_list) - 1) and inx < 0:
+    if idx > (len(my_list) - 1) and idx < 0:
         return None
     else:
-        inx = my_list[idx]
-        return inx
+        idx = my_list[idx]
+        return idx
