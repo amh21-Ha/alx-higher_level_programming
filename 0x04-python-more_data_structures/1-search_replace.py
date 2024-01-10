fcd5bec2bf007218c9cce2c47fb0b4f2d289b10a
Amha <amha.ha07@gmail.com>
2024-01-09 22:43:04 -0800

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = []
+    n_list = []
     for z in range(len(my_list)):
         if my_list[z] == search:
-            new_list.append(replace)
+            n_list.append(replace)
         else:
-            new_list.append(my_list[z])
-    return new_list
+            n_list.append(my_list[z])
+    return n_list
