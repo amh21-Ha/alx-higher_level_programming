5bd7a732c33ac0433409a49898ca3c998d15e4f2
Amha <amha.ha07@gmail.com>
2024-01-09 23:14:56 -0800

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def oman_to_int(roman_string):
+def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
