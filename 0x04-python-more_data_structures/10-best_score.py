8ac7a0e84d507513717baf7b520014e07caca210
Amha <amha.ha07@gmail.com>
2024-01-10 01:13:53 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 def best_score(a_dictionary):
     if a_dictionary is None:
-       return None
+        return None
     new_d = a_dictionary.copy()
     if a_dictionary:
         new_d = max(a_dictionary, key=a_dictionary.get)
