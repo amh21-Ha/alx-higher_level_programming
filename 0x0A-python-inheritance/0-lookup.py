83487e0ae76ce588773fb4f4f3328554d456edd2
Amha <amha.ha07@gmail.com>
2024-02-05 03:17:41 -0800

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+def lookup(obj):
+    """
+    Returns a list of attributes and methods of the given object.
+    param obj: Any Python object
+    :return: List of attribute and method names
+    """
+    return dir(obj)
