b2017dc22fef4e8fa0f219ac1931de87a8bd4875
amha <amha.ha07@gmail.com>
2024-04-25 22:48:19 -0700

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Check if a URL was provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# The URL provided as the first argument
-URL=$1
-
-# Use curl to send a request and pipe the response body to wc to count the bytes
-SIZE=$(curl -s "$URL" | wc -c)
-
-echo "The size of the response body is: $SIZE bytes"
+# sends a request an URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
